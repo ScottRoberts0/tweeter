0414797fee7c8284ca22c895acf70700e236ac8e
Scott Roberts <sroberts.kia@gmail.com>
2019-08-14 00:00:47 +0000

    Daily commit, don't judge me

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -3,10 +3,39 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
+function timeSince(date) {
+  //stolen from stackoverflow https://stackoverflow.com/questions/3177836/how-to-format-time-since-xxx-e-g-4-minutes-ago-similar-to-stack-exchange-site
+  let seconds = Math.floor((new Date() - date) / 1000);
+
+  let interval = Math.floor(seconds / 31536000);
+
+  if (interval > 1) {
+    return interval + " years";
+  }
+  interval = Math.floor(seconds / 2592000);
+  if (interval > 1) {
+    return interval + " months";
+  }
+  interval = Math.floor(seconds / 86400);
+  if (interval > 1) {
+    return interval + " days";
+  }
+  interval = Math.floor(seconds / 3600);
+  if (interval > 1) {
+    return interval + " hours";
+  }
+  interval = Math.floor(seconds / 60);
+  if (interval > 1) {
+    return interval + " minutes";
+  }
+  return Math.floor(seconds) + " seconds";
+}
+
+
 
 const createTweetElement = function (tweetdata) {
-  const now = new Date();
-  const created = tweetdata.created_at;
+
+  const created = timeSince(tweetdata.created_at) + " ago";
   
   const markup = $(`
   <article class="tweet">
@@ -19,7 +48,7 @@ const createTweetElement = function (tweetdata) {
       </p>
   <footer>
     <span>
-      ${now} days ago
+      ${created} 
     </span>
     <span class="tweetcons">
         <i class="fas fa-flag"></i>
@@ -31,31 +60,50 @@ const createTweetElement = function (tweetdata) {
 
   `);
 
-
-
-
-
   return markup;
 }
 
+const renderTweets = function(tweets){
+  $(document).ready(function () {
+   let tweet;
+    for(let key of tweets){
+      console.log(key)
+      tweet = createTweetElement(key)
 
-const tweetData = {
-  "user": {
-    "name": "Newton",
-    "avatars": "https://i.imgur.com/73hZDYK.png",
-    "handle": "@SirIsaac"
-  },
-  "content": {
-    "text": "If I have seen further it is by standing on the shoulders of giants"
-  },
-  "created_at": 1461116232227
+      $('#tweets-container').append(tweet);
+    }
+   
+  });
+
+
 }
 
+const data = [
+  {
+    "user": {
+      "name": "Newton",
+      "avatars": "https://i.imgur.com/73hZDYK.png"
+      ,
+      "handle": "@SirIsaac"
+    },
+    "content": {
+      "text": "If I have seen further it is by standing on the shoulders of giants"
+    },
+    "created_at": 1461116232227
+  },
+  {
+    "user": {
+      "name": "Descartes",
+      "avatars": "https://i.imgur.com/nlhLi3I.png",
+      "handle": "@rd" },
+    "content": {
+      "text": "Je pense , donc je suis"
+    },
+    "created_at": 1461113959088
+  }
+]
 
 
-const $tweet = createTweetElement(tweetData);
-console.log($tweet); // to see what it looks like
-$(document).ready(function () {
-  $('#tweets-container').append($tweet);
-});
-
+renderTweets(data);
+// console.log(new Date(tweetData.created_at))
+// console.log(timeSince(tweetData.created_at));
