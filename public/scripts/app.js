69cb0cc32783ae6952087b910f878164dccd6782
Scott Roberts <sroberts.kia@gmail.com>
2019-08-16 14:09:21 +0000

    tweeked css

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -7,7 +7,7 @@
 
 
 
-function timeSince(date) {
+const timeSince = function(date) {
   //stolen from stackoverflow https://stackoverflow.com/questions/3177836/how-to-format-time-since-xxx-e-g-4-minutes-ago-similar-to-stack-exchange-site
   let seconds = Math.floor((new Date() - date) / 1000);
 
@@ -35,17 +35,14 @@ function timeSince(date) {
   return Math.floor(seconds) + " seconds";
 }
 
-const escape =  function(str) {
+const escape = function (str) {
   let div = document.createElement('div');
   div.appendChild(document.createTextNode(str));
   return div.innerHTML;
 }
 
 const createTweetElement = function (tweetdata) {
-
   const created = timeSince(tweetdata.created_at) + " ago";
-
- 
   const markup = $(`
   <article class="tweet">
   <header>
@@ -72,24 +69,26 @@ const createTweetElement = function (tweetdata) {
 
 
 const renderTweets = function (tweets) {
- 
-    let tweet;
-    $('#tweets-container').empty();
-    for (let key of tweets) {
-      tweet = createTweetElement(key)
-      $('#tweets-container').append(tweet);
-    }
+  let tweet;
+  $('#tweets-container').empty();
+  
+  for (let key of tweets) {
+    tweet = createTweetElement(key)
+    $('#tweets-container').prepend(tweet);
+  }
+  
 }
 
 
-const loadtweets = function(){
-  $.ajax({url: "/tweets", type: "GET"})
-  .then(function(responseData){
-    renderTweets(responseData);
-  });
+const loadtweets = function () {
+  $.ajax({ url: "/tweets", type: "GET" })
+    .then(function (responseData) {
+      renderTweets(responseData);
+    });
 }
 
 $(document).ready(function () {
+  //pageload setup
   loadtweets();
   $('#error').hide();
   $('#topbutton').hide();
@@ -99,46 +98,52 @@ $(document).ready(function () {
     const textareaVal = $("textarea").val();
     const tweetlength = Number($(".counter").text());
     event.preventDefault();
-    
+
     console.log(typeof tweetlength);
-    
-    if(tweetlength < 0){
+
+    if (tweetlength < 0) {
       $('#msg').text("ERROR: Tweet Length Over 140 Chars");
-      $('#error').show();
-    }else if(textareaVal === ""){
+      $('#error').show("slow", () => {
+        $("textarea").focus();
+      });
+    } else if (textareaVal === "") {
       $('#msg').text("ERROR: Tweet is EMPTY");
-      $('#error').show();
-    }
-    else{
+      $('#error').show("slow", () => {
+        $("textarea").focus();
+      });
+    } else {
       $('#error').slideUp();
-    const formData = $form.serialize();
- 
-    console.log('Button clicked, performing ajax call...');
+      const formData = $form.serialize();
 
-    $.ajax({url: "/tweets", type: 'POST', data: formData })
-    .then(function () {
-      loadtweets();
-      console.log('Success!');
+      console.log('Button clicked, performing ajax call...');
+
+      $.ajax({ url: "/tweets", type: 'POST', data: formData })
+        .then(function () {
+          loadtweets();
+          console.log('Success!');
+          $("textarea").val("");
+          $(".counter").html(140);
+        });
+    }
+  });
+
+  $('#tweet-btn').click(function () {
+    $(".new-tweet").toggle("slow", () => {
+      $("textarea").focus();
     });
-  }
   });
 
-  $('#tweet-btn').click(function(){
-    $(".new-tweet").toggle("slow");
-  });
-
-  $(window).scroll(function(){
-    
-    if($(this).scrollTop() > 300){
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 300) {
       $('#topbutton').fadeIn();
-    }else{
+    } else {
       $('#topbutton').fadeOut();
     }
   });
 
-  $('#topbutton').click(function(){
+  $('#topbutton').click(function () {
     window.scrollTo(0, 0);
+    $(".new-tweet").show("slow")
   })
-
 });
 
